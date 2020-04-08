96a4e6749835876b234993a93ef242c7943ea9b4
senovr <serp2006@gmail.com>
2020-04-08 14:54:00 +0300

    notebook added

--- src/data/run_task_downloader_daily.bat
+++ src/data/run_task_downloader_daily.bat
@@ -1,4 +1,5 @@
-call C:\ProgramData\Anaconda3\Scripts\activate.bat C:\ProgramData\Anaconda3\envs\dev_py_376_rul_spod_2
+REM put your own path to the activate.bat and environment below
+call C:\Users\Sergey\Anaconda3\Scripts\activate.bat C:\Users\Sergey\Anaconda3\envs\DL_py378
 call python downloader.py
 for /f "skip=1" %%d in ('wmic os get localdatetime') do if not defined mydate set mydate=%%d
 set dirname_log=%mydate:~0,8%_downloader_log
